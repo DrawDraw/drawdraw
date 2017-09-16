25cfc06f1b2f8e44ba5bed04316690a49db760f4
Anson Ng <cwnga@yahoo-inc.com>
2017-09-16 17:01:11 +0800

    add delete tmp file

--- routes/image/upload.js
+++ routes/image/upload.js
@@ -9,6 +9,9 @@ router.post('/', function(req, res, next) {
     var form = new formidable.IncomingForm();
     form.parse(req, function(err, fields, files) {
 	    imageUploader.uploadImage(files.file.path, function (err, result) { 
+            var fs = require('fs');
+            fs.unlink(files.file.path,function(err){
+            });
 	   	    var returnJson = {"imageUrl": result};
 	   	    res.status(200).json(returnJson);
         });
