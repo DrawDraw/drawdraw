4beb07f81aee613844380b301da66ccb8dcdbfc9
Anson Ng <cwnga@yahoo-inc.com>
2017-10-22 17:39:49 +0800

    add ERROR CODE

--- models/DDError.js
+++ models/DDError.js
@@ -1,10 +1,12 @@
 var DDError = {}
 
+DDError.ERRORCODE_VALIDATE = "E0001";
+DDError.ERRORCODE_SYSTEM = "E9999";
+
 DDError.create = function(code, message) {
 return {
          "code": code, 
          "meessage": message
        };
 }
-
 module.exports = DDError;
