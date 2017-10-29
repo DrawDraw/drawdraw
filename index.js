e2d0a887d5b02a4ec0fa74db8eef4ad3673d1146
Anson Ng <cwnga@yahoo-inc.com>
2017-10-29 18:16:33 +0800

    remove /me path, add users

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
 var upload = require(__dirname + '/routes/api/v1/image/upload'); //image upload
-var me = require(__dirname + '/routes/api/v1/me/me'); 
+var user = require(__dirname + '/routes/api/v1/users/user');
+
 
 var webLogin = require(__dirname + '/routes/web/login'); //image upload
 
 app.use('/api/v1/images', upload);
-app.use('/api/v1/me', me);
+app.use('/api/v1/users', user);
 app.use('/web/login', webLogin);
 
 app.set('port', (process.env.PORT || 5000));
