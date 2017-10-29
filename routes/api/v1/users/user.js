abba6e1d9a8d03392eeacd26ccbe8bfe43cd1c8b
Anson Ng <cwnga@yahoo-inc.com>
2017-10-29 18:22:30 +0800

    indent

--- routes/api/v1/users/user.js
+++ routes/api/v1/users/user.js
@@ -5,18 +5,18 @@ var service = require(__dirname + '/../../../../libs/DDService.js');
 /* GET home page. */
 router.get('/me', function(req, res, next) {
     service.getUserWithExternalType(req.query.token, req.query.type, function(ddError, ddUser) {
-       if (ddError) {
-           res.setHeader('Content-Type', 'application/json');
-           res.status(400).json(ddError);
-       } else {
-           res.setHeader('Content-Type', 'application/json');
-           res.status(200).json(ddUser);
-       }
+        if (ddError) {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(400).json(ddError);
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(ddUser);
+        }
     });
 });
 
 router.get('/:userId', function(req, res, next) {
-service.getUserWithId(req.params.userId,  function(ddError, ddUser) {
+    service.getUserWithId(req.params.userId,  function(ddError, ddUser) {
         if (ddError) {
            res.setHeader('Content-Type', 'application/json');
            res.status(400).json(ddError);
